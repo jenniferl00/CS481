17d010e5eb25e86c4ed5e7fdf24c17691ff8f279
jenniferl00 <73903034+jenniferl00@users.noreply.github.com>
2021-05-18 09:53:21 -0700

    Create MyPetsActivity.kt

---
+++ Healthy Paws/app/java/com.example.healthypaws/MyPetsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.healthypaws
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MyPetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_pets)
+
+        val allPetsBack = findViewById<Button>(R.id.MyPetsPageBack);
+
+        allPetsBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
